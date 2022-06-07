90357c0f2719dd83a0f2489034e873f5b7096109
clemfavre <clemm.favre@gmail.com>
2022-06-08 00:38:47 +0200

    change the structure to work with an array

--- Game of Life.py
+++ Game of Life.py
@@ -9,52 +9,52 @@ from math import*
 from colorama import Fore,Style
 import time
 
-# defdeparture
-# 0,1,0,0,1,0,0,0,0,0
-# 1,1,1,1,0,1,0,0,0,0
-# 1,0,0,1,1,0,1,0,1,1
-# 1,1,0,0,1,0,0,0,1,0
-# 1,0,0,1,0,1,0,0,1,1
-# 0,0,1,1,0,0,0,0,1,0
-# 0,0,0,1,0,1,0,1,0,1
-# 1,0,1,0,0,0,0,0,0,0
-# 0,1,1,1,0,1,1,0,1,0
-# 0,0,1,1,0,0,1,1,0,0
-# 0,0,0,0,0,0,0,0,0,0,0
+
 
 # defdeparture
-L=[0,1,0,0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,1,0,0,1,1,0,1,0,1,1,1,1,0,0,1,0,0,0,1,0,1,0,0,1,0,1,0,0,1,1,0,0,1,1,0,0,0,0,1,0,0,0,0,1,0,1,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
-Lafter=[0,1,0,0,1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,1,0,0,1,1,0,1,0,1,1,1,1,0,0,1,0,0,0,1,0,1,0,0,1,0,1,0,0,1,1,0,0,1,1,0,0,0,0,1,0,0,0,0,1,0,1,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,1,1,1,0,1,1,0,1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
+grid=[[0,0,1,0,0,1,0,0,0,0,0,0],[0,1,1,1,1,0,1,0,0,0,0,0],[0,1,0,0,1,1,0,1,0,1,1,0],[0,1,1,0,0,1,0,0,0,1,0,0],[0,1,0,0,1,0,1,0,0,1,1,0],[0,0,0,1,1,0,0,0,0,1,0,0],[0,0,0,0,1,0,1,0,1,0,1,0],[0,1,0,1,0,0,0,0,0,0,0,0],[0,0,1,1,1,0,1,1,0,1,0,0],[0,0,0,1,1,0,0,1,1,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0]]
+lines=10 # without the last line that is full of dead cells and that doesn't change of state
+columns=10 # without the first and the last columns that are full of dead cells and that don't change of state
+# more visually
+# 0,0,1,0,0,1,0,0,0,0,0,0
+# 0,1,1,1,1,0,1,0,0,0,0,0
+# 0,1,0,0,1,1,0,1,0,1,1,0
+# 0,1,1,0,0,1,0,0,0,1,0,0
+# 0,1,0,0,1,0,1,0,0,1,1,0
+# 0,0,0,1,1,0,0,0,0,1,0,0
+# 0,0,0,0,1,0,1,0,1,0,1,0
+# 0,1,0,1,0,0,0,0,0,0,0,0
+# 0,0,1,1,1,0,1,1,0,1,0,0
+# 0,0,0,1,1,0,0,1,1,0,0,0
+# 0,0,0,0,0,0,0,0,0,0,0,0
 
-# L[-11]=L[100]=[0]
 
 
 def nextstep():
-    L=Lafter
-    for k in range(0,99):
-        cells=0
-        if L[k-11]==1:
-            cells=cells+1
-        if L[k-10]==1:
-            cells=cells+1
-        if L[k-9]==1:
-            cells=cells+1
-        if L[k-1]==1:
-            cells=cells+1
-        if L[k+1]==1:
-            cells=cells+1
-        if L[k+9]==1:
-            cells=cells+1
-        if L[k+10]==1:
-            cells=cells+1
-        if L[k+11]==1:
-            cells=cells+1
-        if cells<2 or cells>3:
-            Lafter[k]=0
-        if cells==3:
-            Lafter[k]=1
-    return(Lafter)
-
+    for i in range(0,lines):
+        for j in range(1,columns+1):
+            cells=0
+            if grid[i-1][j-1]==1:
+                cells=cells+1
+            if grid[i-1][j]==1:
+                cells=cells+1
+            if grid[i-1][j+1]==1:
+                cells=cells+1
+            if grid[i][j-1]==1:
+                cells=cells+1
+            if grid[i][j+1]==1:
+                cells=cells+1
+            if grid[i+1][j-1]==1:
+                cells=cells+1
+            if grid[i+1][j]==1:
+                cells=cells+1
+            if grid[i+1][j+1]==1:
+                cells=cells+1
+            if cells<2 or cells>3:
+                grid[i][j]=0
+            if cells==3:
+                grid[i][j]=1
+    return(grid)
 #print(nextstep())
 
 
@@ -65,41 +65,9 @@ def pause(p):
 
 
 def display():
-    L=Lafter #bizzare, je crois que ca devrait etre supprime
-    for k in range(0,10):
-        i=10*k
-        Lk=[]
-        Lk=Lk+[L[i]]+[L[i+1]]+[L[i+2]]+[L[i+3]]+[L[i+4]]+[L[i+5]]+[L[i+6]]+[L[i+7]]+[L[i+8]]+[L[i+9]]
-        if k==0:
-            L0=Lk
-        elif k==1:
-            L1=Lk
-        elif k==2:
-            L2=Lk
-        elif k==3:
-            L3=Lk
-        elif k==4:
-            L4=Lk
-        elif k==5:
-            L5=Lk
-        elif k==6:
-            L6=Lk
-        elif k==7:
-            L7=Lk
-        elif k==8:
-            L8=Lk
-        else:
-            L9=Lk
-    print(L0)
-    print(L1)
-    print(L2)
-    print(L3)
-    print(L4)
-    print(L5)
-    print(L6)
-    print(L7)
-    print(L8)
-    print(L9)
+    for k in range(0,lines+1):
+        print(grid[k])
+    print("space")
 #display()
 
 def main():
@@ -110,46 +78,6 @@ def main():
         nextstep()
         pause(p)
 main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
